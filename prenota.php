157ba34d5f4ffc943824b8c346320900ad4589ae
LauraVasile <56387015+LauraVasile@users.noreply.github.com>
2021-03-06 11:49:34 +0100

    Add configuration about errors

--- prenota.php
+++ prenota.php
@@ -1,2 +1,7 @@
 <?php
-ini_set("DELBUG", "1");
+// dice a livello dello script che gli errori verranno mostrati e che non verranno loggati
+ini_set('display_errors', 1);
+ini_set('log_errors', 0);
+
+
+

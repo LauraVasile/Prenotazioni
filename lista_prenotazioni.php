f0b46bb7e83db111ee41e0c10503653d1bc4a4d0
LauraVasile <56387015+LauraVasile@users.noreply.github.com>
2021-03-12 15:53:04 +0100

    Add table to sort the list of reservations

--- lista_prenotazioni.php
+++ lista_prenotazioni.php
@@ -20,7 +20,33 @@ $pdo = new PDO($dsn, $user, $pass);
 $sql = "SELECT * FROM prenotazioni";
 $stmt = $pdo-> query($sql);
 
-$result = $stmt-> fetchAll();
-echo "<pre>";
-var_dump($result);
-echo "</pre>";
+//$result = $stmt-> fetchAll();
+
+echo '<table>
+<tr>
+<td> Id</td>
+<td> codice fiscale</td>
+<td> Data</td>
+</tr>';
+
+$stmt  = $pdo -> query($sql);
+while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+    $id = $row['id'];
+    $codice_fiscale = $row['codice_fiscale'];
+    $data = $row['giorno'];
+    echo "<tr>
+          <td>$id</td>
+          <td>$codice_fiscale</td>
+          <td>$data</td>
+          </tr>";
+}
+echo '</table>';
+
+
+
+echo "
+<table>
+    <td></td>
+</table>";
+
+

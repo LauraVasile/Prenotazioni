ca8d43cd68364f112babe01d8bd474636e649272
LauraVasile <56387015+LauraVasile@users.noreply.github.com>
2021-03-18 11:51:04 +0100

    Add template and table

--- prenota.php
+++ prenota.php
@@ -1,31 +1,34 @@
 <?php
-// dice a livello dello script che gli errori verranno mostrati e che non verranno loggati
+// Dice a livello dello script che gli errori verranno mostrati e che non verranno loggati
 include_once "config.php";
 
 // $sql = "SELECT * FROM prenotazioni WHERE giorno = 2021-06-03";
 
- // variabili valorizzate tramite POST
+ // Variabili valorizzate tramite POST
 $codice_fiscale = $_POST['codice'];
 $giorno = $_POST['giorno'];
+$codice_prenotazione=uniqid(rand(),true);
 
+//Query di inserimento preparate
+$sql = "INSERT INTO prenotazioni VALUES(null, :codice_fiscale, :giorno, :codice_prenotazione)";
 
-// Query di inserimento preparata
-$sql = "INSERT INTO prenotazioni VALUES(null, :codice_fiscale, :giorno)";
 
 //inviamo la query al database che la tiene in pancia
 $stmt = $pdo->prepare($sql);
 
 //inviamo i dati concreti che verranno messi al posto dei segnaposto
 
-$stmt -> execute(
+$stmt->execute(
     [
         'codice_fiscale' => $codice_fiscale,
-        'giorno' => $giorno
-    ]
+        'giorno' => $giorno,
+    'codice_prenotazione' => $codice_prenotazione
+]
 );
- // query preparate servono per passare i valori delle variabili
+ //Query preparate servono per passare i valori delle variabili
 
-//spedire delle righe nell'header dell'http
+//Spedire delle righe nell'header dell'http
 // Ridirige il browser verso la pagina indicata nella location
-header('Location:lista_prenotazioni.php'); // risposta che arriva al browser
+
+header('Location:lista_prenotazioni.php'); // Risposta che arriva al browser
 exit(0);  // fa finire lo script
